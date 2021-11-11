0423e48c06e6123602115342273dd129382703de
iann <502975759@qq.com>
2021-11-11 15:59:52 +0800

    test: gh-page

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,22 @@
-const path = require('path');
-const resolve = dir => path.join(__dirname, dir);
-
+const path = require("path");
+const resolve = (dir) => path.join(__dirname, dir);
 
 module.exports = {
-    assetsDir: 'examples/assets/',
-    filenameHashing: false,
-    pages: {
-        index: {
-            entry: 'examples/main.js',
-            template: 'public/index.html',
-            filename: 'index.html',
-        }
+  assetsDir: "./examples",
+  filenameHashing: false,
+  pages: {
+    index: {
+      entry: "examples/main.js",
+      template: "public/index.html",
+      filename: "index.html",
     },
-    chainWebpack: config => {
-        config.resolve.alias
-        .set('@', resolve('examples'))
-        .set('@visam', resolve('packages'));
-
-    },
-    // devServer: {
-    //     host: '0.0.0.0'
-    // }
-}
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set("@", resolve("examples"))
+      .set("@visam", resolve("packages"));
+  },
+  // devServer: {
+  //     host: '0.0.0.0'
+  // }
+};
