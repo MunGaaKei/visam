d2641ad94687213825433673748a3771d4b87e71
iann <502975759@qq.com>
2023-02-24 02:18:31 +0800

    fix: update dev dependencies

--- packages/components/notice/notice.vue
+++ packages/components/notice/notice.vue
@@ -1,26 +1,35 @@
 <template>
-  <div class="vsm-notice"
-    :class="[{
-      'vsm-on': show,
-      'vsm-reverse': reverse
-    }, color? `vsm-${color}`: '']"
-    :style="position"
-    @click="handleClick">
-      <div v-if="html" v-html="html"></div>
-      <a v-if="closable === 'button'" class="vsm-close" @click.stop="handleClose"></a>
-  </div>
+    <div
+        class="vsm-notice"
+        :class="[
+            {
+                'vsm-on': show,
+                'vsm-reverse': reverse,
+            },
+            color ? `vsm-${color}` : '',
+        ]"
+        :style="position"
+        @click="handleClick"
+    >
+        <div v-if="html" v-html="html"></div>
+        <a
+            v-if="closable === 'button'"
+            class="vsm-close"
+            @click.stop="handleClose"
+        ></a>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'vsmNotice',
+    name: "vsmNotice",
     methods: {
-      handleClick () {
-        this.closable === 'self' && this.handleClose();
-      },
-      handleClose () {
-        this.closable && this.close();
-      }
+        handleClick() {
+            this.closable === "self" && this.handleClose();
+        },
+        handleClose() {
+            this.closable && this.close();
+        },
     },
-}
+};
 </script>
